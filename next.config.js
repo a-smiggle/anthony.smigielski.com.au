935a93dbd7bbf248bf0f607db2c139d5dd268652
a-smiggle <anthony.smigielski@protonmail.com>
2022-07-22 06:44:59 +0800

    Dockerize
    Add docker file for local deployment.
    Will not deploy to GH Pages as static builds do not work.
    
    a-smiggle

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  assetPrefix: isProd ? '/anthony.smigielski.com.au/' : '',
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
